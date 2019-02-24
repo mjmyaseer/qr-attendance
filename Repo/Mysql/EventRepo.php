c698cacd6953e5284761d8381298e66c9efcec86
yaseer <mjmyaseer@gmail.com>
2019-02-24 14:15:25 +0530

    commit before demo

--- Repo/Mysql/EventRepo.php
+++ Repo/Mysql/EventRepo.php
@@ -226,12 +226,8 @@ class EventRepo implements EventInterface
             ->where(UserEvent::TABLE . '.customer_id', '=', $request);
         $results = $query->get();
 
-        $event = $this->userEvent->where('customer_id', $request)->get();
-
-
-        dd($event);
-
-
+//        $event = $this->userEvent->where('customer_id', $request)->get();
+//        dd($event);
 //        $uid = strtoupper(bin2hex(openssl_random_pseudo_bytes(16)));
 //        $ids = "event_id = " . $request->event_id . ",user_id = " . $request->user_id . ",unique_id = " . $uid;
 //        $event->qr_code = base64_encode(QrCode::format('png')->size(100)->generate($ids));
